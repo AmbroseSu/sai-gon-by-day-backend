4aa0a8ebc3ad6d2851cd444c328c83416461d00b
AmbroseSu <hieuhcse173055@fpt.edu.vn>
2024-05-27 21:42:53 +0700

    first_commit

---
+++ src/main/java/com/ambrose/saigonbyday/SaiGonByDayApplication.java
@@ -0,0 +1,13 @@
+package com.ambrose.saigonbyday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaiGonByDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaiGonByDayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ambrose/saigonbyday/SaiGonByDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ambrose.saigonbyday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaiGonByDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
