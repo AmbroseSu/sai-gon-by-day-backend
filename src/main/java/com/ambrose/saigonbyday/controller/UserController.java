8c47dc7bbfb4896ab7270c11ca487951559936c5
AmbroseSu <ambrose472263@gmail.com>
2024-07-14 22:32:59 +0700

    add_cross

--- src/main/java/com/ambrose/saigonbyday/controller/UserController.java
+++ src/main/java/com/ambrose/saigonbyday/controller/UserController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 @PreAuthorize("hasAuthority('CUSTOMER')")
+@CrossOrigin
 public class UserController {
   //private final UserService userService;
   @Autowired
